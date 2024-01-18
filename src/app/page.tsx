344136b0fcb66cca56b429ed0909cb3c9a7d8d7c
Vimal Govind <vimalgovind@live.in>
2024-01-18 16:21:47 +0300

    install latest npm and next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -172,7 +172,7 @@ export default function Page() {
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
             {RESUME_DATA.projects.map((project) => {
               return (
-                <ProjectCard
+                project && <ProjectCard
                   key={project.title}
                   title={project.title}
                   description={project.description}
